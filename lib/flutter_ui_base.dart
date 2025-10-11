c2a07ebcc0fb86dab5f31cb900887d356d957fbf
vraj0703 <vraj0703@gmail.com>
2025-10-11 13:12:43 +0530

    remove getit

--- lib/flutter_ui_base.dart
+++ lib/flutter_ui_base.dart
@@ -1,19 +1,15 @@
-import 'package:injectable/injectable.dart';
-
 import 'flutter_ui_base_platform_interface.dart';
-import 'package:get_it/get_it.dart';
 import 'package:my_localizations/library.dart';
 import 'package:my_theme_style/styles/styles.dart' show AppStyle;
-
-@InjectableInit()
-Future<void> configureDependencies(String flavor) async =>
-    GetIt.instance;
 
 /// A Flutter plugin that initializes with AppLocalizations and AppStyle.
 ///
 /// This class provides a centralized point for accessing localization strings
 /// and application styles. It utilizes GetIt for dependency injection.
 class FlutterUiBase {
+  static late AppLocalizations _localizations;
+  static late AppStyle _appStyle;
+
   /// Initializes the FlutterBase plugin.
   ///
   /// This method should be called once during app startup, typically in `main.dart`.
@@ -22,16 +18,20 @@ class FlutterUiBase {
     required AppStyle style,
     required AppLocalizations localizations,
   }) async {
-    GetIt.I.registerSingleton<AppStyle>(style);
-    GetIt.I.registerSingleton<AppLocalizations>(localizations);
+    _localizations = localizations;
+    _appStyle = style;
   }
 
   Future<String?> getPlatformVersion() {
     return FlutterUiBasePlatform.instance.getPlatformVersion();
   }
+
+  static AppLocalizations get localizations => _localizations;
+
+  static AppStyle get appStyle => _appStyle;
 }
 
 /// Global helpers for readability
-AppLocalizations get $strings => GetIt.I.get<AppLocalizations>();
+AppLocalizations get $strings => FlutterUiBase.localizations;
 
-AppStyle get $styles => GetIt.I.get<AppStyle>();
+AppStyle get $styles => FlutterUiBase.appStyle;
